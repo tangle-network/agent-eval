fd03ee49e2d863c8d0676a5c217195b02994d486
Drew Stone <drewstone329@gmail.com>
2026-07-21 15:11:51 -0600

    chore(release): bump to 0.123.0

--- clients/python/src/agent_eval_rpc/__init__.py
+++ clients/python/src/agent_eval_rpc/__init__.py
@@ -58,7 +58,7 @@ from .models import (
 try:
     __version__ = version("agent-eval-rpc")
 except PackageNotFoundError:
-    __version__ = "0.122.9"
+    __version__ = "0.123.0"
 
 __all__ = [
     "Client",
