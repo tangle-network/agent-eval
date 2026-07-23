1914b5dc05e720d87af2d35ef05ca66b839451c9
Drew Stone <drewstone329@gmail.com>
2026-07-23 17:10:21 -0600

    fix(release): publish GEPA bridge on PyPI

--- clients/python/src/agent_eval_rpc/__init__.py
+++ clients/python/src/agent_eval_rpc/__init__.py
@@ -58,7 +58,7 @@ from .models import (
 try:
     __version__ = version("agent-eval-rpc")
 except PackageNotFoundError:
-    __version__ = "0.123.6"
+    __version__ = "0.123.7"
 
 __all__ = [
     "Client",
