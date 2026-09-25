27514e24406bd2690f3a557889b2dfa4771ba52d
Drew Stone <drewstone329@gmail.com>
2026-09-25 02:40:52 -0700

    chore(release): prepare agent-eval 0.190.0

--- clients/python/src/agent_eval_rpc/__init__.py
+++ clients/python/src/agent_eval_rpc/__init__.py
@@ -53,7 +53,7 @@ from .models import (
 try:
     __version__ = version("agent-eval-rpc")
 except PackageNotFoundError:
-    __version__ = "0.189.0"
+    __version__ = "0.190.0"
 
 __all__ = [
     "Client",
